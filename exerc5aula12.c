2d8238f7aa412732cc775a15defb04483b5ee012
Ana Carolina <acarolbrianez@hotmail.com>
2025-04-30 19:57:50 -0300

    Add files via upload

---
+++ exerc5aula12.c
@@ -0,0 +1,20 @@
+// exercicio 5 - aula 12:
+// Crie um programa que recebe uma senha e determina se ela possui 8 ou mais caracteres. Se for menor que 8 mostre a mensagem: "Erro na senha". Se for maior ou igual a 8 imprima: "Senha conforme".
+
+include <stdio.h>
+
+int main() {
+    char senha[100];
+    printf("Digite uma senha: ");
+    scanf("%s", senha);
+    int tamanho = 0;
+    while (senha[tamanho] != '\0') {
+        tamanho++;
+    }
+    if (tamanho >= 8) {
+        printf("Senha conforme\n");
+    } else {
+        printf("Erro na senha\n");
+    }
+    return 0;
+}
