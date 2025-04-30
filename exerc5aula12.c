08406400a737173e139f838946ec9faef6e46301
Ana Carolina <acarolbrianez@hotmail.com>
2025-04-30 20:13:38 -0300

    Update exerc5aula12.c

--- exerc5aula12.c
+++ exerc5aula12.c
@@ -1,7 +1,7 @@
 // exercicio 5 - aula 12:
 // Crie um programa que recebe uma senha e determina se ela possui 8 ou mais caracteres. Se for menor que 8 mostre a mensagem: "Erro na senha". Se for maior ou igual a 8 imprima: "Senha conforme".
 
-include <stdio.h>
+#include <stdio.h>
 
 int main() {
     char senha[100];
